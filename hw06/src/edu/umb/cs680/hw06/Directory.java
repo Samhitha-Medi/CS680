e8a95d5225287fddbe792fd57962a66ffe66411a
Samhitha-Medi <113398002+Samhitha-Medi@users.noreply.github.com>
2023-05-20 18:31:44 -0400

    HW-06

---
+++ hw06/src/edu/umb/cs680/hw06/Directory.java
@@ -0,0 +1,74 @@
+package edu.umb.cs680.hw06;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+public class Directory extends FSElement {
+    private LinkedList<FSElement> children=new LinkedList<>();
+
+
+    Directory(Directory parent, String name, int size, LocalDateTime creationTime) {
+        super(parent, name, 0, creationTime);
+    }
+    LinkedList<FSElement> getChildren(){
+        return children;
+
+    }
+    void appendChild(FSElement child){
+        this.children.add(child);
+        //child.setParent(this);
+
+    }
+    int countChildren(){
+        int fcount=0,dcount=0;
+        for(FSElement a:getChildren())
+        {
+            if(a.isFile())
+            {
+                fcount++;
+            } else if (a.isDirectory()) {
+                dcount++;
+            }
+        }
+    int c=fcount+dcount;
+        return c;
+    }
+    LinkedList<Directory> getSubDirectories(){
+        LinkedList<Directory>subd=new LinkedList<Directory>();
+        for(FSElement a:getChildren())
+            if(a instanceof Directory)
+                subd.add((Directory)a);
+        return subd;
+
+    }
+
+    LinkedList<File> getFiles(){
+        LinkedList<File>f=new LinkedList<File>();
+        for(FSElement a:getChildren())
+            if(a instanceof File)
+                f.add((File)a);
+        return f;
+
+    }
+    int getTotalSize(){
+        int count=0;
+        for(FSElement a:getChildren()){
+            if(a instanceof File){
+                count+=a.getSize();
+            }
+            else {
+                count+=((Directory)a).getTotalSize();
+            }
+        }
+        return count;
+    }
+    @Override
+    public boolean isDirectory() {
+        return true;
+    }
+
+    @Override
+    public boolean isFile() {
+        return false;
+    }
+
+}
---
+++ hw06/test/src/edu/umb/cs680/hw06/DirectoryTest.java
@@ -0,0 +1,71 @@
+package edu.umb.cs680.hw06;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectoryTest  {
+    static FileSystem fs;
+    @BeforeAll
+    public static void setUpFs(){
+       fs=TextFixtureInitialiser.createFS() ;
+    }
+
+    private String[] dirToStringArray(Directory d){
+        String dirInfo[]={d.getName(), String.valueOf(d.getSize()), String.valueOf(d.getCreationTime())};
+        return dirInfo;
+    }
+
+    @Test
+    public void verifydirectoryEquality(){
+        String[]  ex1={"prjRoot","0",String.valueOf(TextFixtureInitialiser.lt)};
+        Directory ac1=TextFixtureInitialiser.fs.getRootDirs().getFirst();
+        assertArrayEquals(ex1,dirToStringArray(ac1));
+    }
+
+    @Test
+    public void test_getChildren(){
+        Directory d1=TextFixtureInitialiser.fs.getRootDirs().get(0);
+        LinkedList<FSElement>cd=d1.getChildren();
+        String ex[]={"src","lib","test","x"};
+        assertEquals(ex[2],cd.get(2).getName());
+    }
+
+    @Test
+    public void test_countChildren(){
+        Directory b1=TextFixtureInitialiser.fs.getRootDirs().get(0);
+        int exp=4;
+        assertEquals(exp,b1.getChildren().size());
+    }
+
+    @Test
+    public void test_getTotalsize(){
+        Directory d1=TextFixtureInitialiser.fs.getRootDirs().get(0);
+        assertEquals(150,d1.getTotalSize());
+    }
+
+    @Test
+    public void test_isDirectory(){
+        Directory d1=fs.getRootDirs().get(0);
+        assertTrue(d1.isDirectory());
+    }
+
+    @Test
+    public void test_getSubd(){
+        Directory d1=fs.getRootDirs().get(0);
+        LinkedList<Directory> sd=d1.getSubDirectories();
+        assertEquals("lib",sd.get(1).getName());
+    }
+
+    @Test
+    public void test_getFile(){
+        Directory d1=fs.getRootDirs().get(0);
+        LinkedList<Directory> sd=d1.getSubDirectories();
+        LinkedList<File> f=sd.get(0).getFiles();
+        assertEquals("a",f.get(0).getName());
+
+    }
+
+}
